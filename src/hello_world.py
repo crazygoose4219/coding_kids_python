6b3a80d3d095bdb369c798ea131a34363a0f1fcd
Kazuki Wright <kazukiwright@gmail.com>
2021-05-03 23:28:43 -0600

    WIP

--- src/hello_world.py
+++ src/hello_world.py
@@ -2,11 +2,11 @@
 # Below line of code prints or outputs a string value:
 # Hi Python
 # print ("hi python!") this is first line of code.
-#print ("I'm so happy to code python! :) :D :3")
-#print ('I\'m so happy to code python! :) :D :3')
-#print ("once was a penguin who ate cheese and more \ncheese \n this is fun, \n coding is very fun")
-#penguin = "penguin want cheese"
-#print (penguin)
+# print ("I'm so happy to code python! :) :D :3")
+# print ('I\'m so happy to code python! :) :D :3')
+# print ("once was a penguin who ate cheese and more \ncheese \n this is fun, \n coding is very fun")
+# penguin = "penguin want cheese"
+# print (penguin)
 # Favirite_number = 3
 # print(Favirite_number)
 
@@ -15,8 +15,9 @@
 # print(other_number_type)
 
 
-#food = 'CHEESE'
-#print(f"This is some {food}"
+# food = 'CHEESE'
+# print(f"This is some {food}"
 derp_1 = "cheese"
 derp = "penguin"
-print (f"the {derp} monches and obliterates the {derp_1}")
+print(f"the {derp} monches and obliterates the {derp_1}")
+
